e9ff5f19424ce9aa0cf7ae0356f0fa2d7d3d4a2e
CommanderVince <vincent_mueller@yahoo.de>
2017-03-13 11:14:38 +0100

    Irgendwas Cooles

---
+++ ExamMaster/ExamMaster/Database/DB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace ExamMaster.Database
+{
+    public class DB:IDisposable
+    {
+        DataTable dataTable = new DataTable();
+
+        public void GetDataTest()
+        {
+        }
+        public void GetData()
+        {
+            string connectionString = "datasource=localhost;database=t_sbf_binnen;username=root;password=";
+            string query = "select * from t_sbf_binnen";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+
+            // create data adapter
+            SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+            // this will query your database and return the result to your datatable
+            adapt.Fill(dataTable);
+            connection.Close();
+            adapt.Dispose();
+        }
+
+        
+        public void Dispose()
+        {
+
+        }
+    }
+}
